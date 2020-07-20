60ff7a96ab12905f1302e79748929c098640ced2
Marco Feng <fhalcyon@gmail.com>
2020-07-20 21:53:33 +0800

    1. Two Sum (Approach 1: Brute Force)

---
+++ Week_01/num_1/TwoSum.java
@@ -0,0 +1,21 @@
+class TwoSum{
+    /**
+     * Approach 1: Brute Force
+     * Time complexity: O(n^2)
+     * Space complexity: O(1)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int sum = nums[i] + nums[j];
+                if (sum == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}

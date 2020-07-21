0610d87ecf31a04a17c04e1a20f2d39b8445f953
Marco Feng <fhalcyon@gmail.com>
2020-07-21 15:34:09 +0800

    1. Two Sum (Approach 2 & 3)

--- Week_01/num_1/TwoSum.java
+++ Week_01/num_1/TwoSum.java
@@ -18,4 +18,47 @@ class TwoSum{
         }
         return null;
     }
+
+    /**
+     * Approach 2: Two-pass Hash Table
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> hm = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            hm.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int key = target - nums[i];
+            if (hm.containsKey(key) && hm.get(key) != i) {
+                return new int[] {i, hm.get(key)};
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Approach 3: One-pass Hash Table
+     * Time complexity: O(n)
+     * Space complexity: O(1)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum3(int[] nums, int target) {
+        Map<Integer, Integer> hm = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            int key = target - nums[i];
+            if (hm.containsKey(key)) {
+                return new int[] {hm.get(key), i};
+            }
+            hm.put(nums[i], i);
+        }
+        return null;
+    }
 }

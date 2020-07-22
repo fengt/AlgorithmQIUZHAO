448197b575c99411b75729b08ec06668eaae8e0c
Marco Feng <fhalcyon@gmail.com>
2020-07-22 22:01:07 +0800

    49. Group Anagrams

---
+++ Week_01/num_49/GroupAnagrams.java
@@ -0,0 +1,15 @@
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0 ) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] ca = str.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!map.containsKey(key)) map.put(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
